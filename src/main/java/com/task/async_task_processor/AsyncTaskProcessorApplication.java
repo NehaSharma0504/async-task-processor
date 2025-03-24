25ffc2635440bdcef76e574427615b73e084c155
neha_sharma <nsneha.sharma804@gmail.com>
2025-03-24 21:03:01 +0530

    initial commit

---
+++ src/main/java/com/task/async_task_processor/AsyncTaskProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.task.async_task_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncTaskProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/async_task_processor/AsyncTaskProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.async_task_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTaskProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
